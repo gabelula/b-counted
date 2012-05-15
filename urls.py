5adfd4114a3f47b2cc1fd9317e3f560d81ad6bd4
Gabriela gaba Rodriguez <gabelula@gmail.com>
2012-05-14 23:44:01 -0700

    Plain urls.py for the migraiton.

--- urls.py
+++ urls.py
@@ -27,49 +27,49 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
  	
-	(r'^$', 'counter.views.CountMe'),
-	(r'^navi/', 'counter.views.navi'),
-    (r'^CountMe/$', 'counter.views.CountMe'),
-    (r'^CountNow/$','counter.views.CountNow'),
-    (r'^CountNow1/$','counter.views.CountNow1'),
-    (r'^about/$','counter.views.about'),
-    
-    (r'^about_me/$','counter.views.about_me'),
-    
-    (r'^addsessionform/$','counter.views.addsessionform'),
-    
-    (r'^addstats/$','counter.views.addstats'),
-    (r'^addstatsform/$','counter.views.addstatsform'),
-    (r'^addmorestats/$','counter.views.addmorestats'),
-    
-    (r'^api/$','counter.views.api'),
-    
-    (r'^delete/$', 'counter.views.delete_it'),
-    (r'^delete_stat/$', 'counter.views.delete_stat'),
-    (r'^delete_address/(?P<address_key>[^\.^/]+)/$', 'counter.views.delete_address'),
-    
-    (r'^feed/$','counter.views.feed'),
-    (r'^Geotag/$','counter.views.Geotag'),
-    
-    (r'^recent/$','counter.views.recently_updated'),
-    
-    (r'^event/(?P<event_key>[^\.^/]+)/$', 'counter.views.event_detail'),
-    (r'^showstats/(?P<event_key>[^\.^/]+)/$', 'counter.views.showstats'),
-    (r'^stat/(?P<stat_key>[^\.^/]+)/$', 'counter.views.stat'),
-   
-   	(r'^contributor_stats/$', 'counter.views.Show_ContributorStat'),
-    
-    (r'^multisession-event/(?P<event_key>[^\.^/]+)/$', 'counter.views.multisession_event_detail'),	
-    (r'^multisession-ratio/(?P<event_key>[^\.^/]+)/$', 'counter.views.multisession_event_ratio'),
-	(r'^all_multisession/$', 'counter.views.all_multisession_events'),
-	(r'^all_multisession_add/$', 'counter.views.all_multisession_events_add'),
-	(r'^all/$', 'counter.views.all_events'),
-	(r'^all/$', 'counter.views.all_events'),
-	(r'^why/$',  'counter.views.why'),
-	(r'^faq/$',  'counter.views.faq'),
-	
-	
-
+#	(r'^$', 'counter.views.CountMe'),
+#	(r'^navi/', 'counter.views.navi'),
+#    (r'^CountMe/$', 'counter.views.CountMe'),
+#    (r'^CountNow/$','counter.views.CountNow'),
+#    (r'^CountNow1/$','counter.views.CountNow1'),
+#    (r'^about/$','counter.views.about'),
+#    
+#    (r'^about_me/$','counter.views.about_me'),
+#    
+#    (r'^addsessionform/$','counter.views.addsessionform'),
+#    
+#    (r'^addstats/$','counter.views.addstats'),
+#    (r'^addstatsform/$','counter.views.addstatsform'),
+#    (r'^addmorestats/$','counter.views.addmorestats'),
+#    
+#    (r'^api/$','counter.views.api'),
+#    
+#    (r'^delete/$', 'counter.views.delete_it'),
+#    (r'^delete_stat/$', 'counter.views.delete_stat'),
+#    (r'^delete_address/(?P<address_key>[^\.^/]+)/$', 'counter.views.delete_address'),
+#    
+#    (r'^feed/$','counter.views.feed'),
+#    (r'^Geotag/$','counter.views.Geotag'),
+#    
+#    (r'^recent/$','counter.views.recently_updated'),
+#    
+#    (r'^event/(?P<event_key>[^\.^/]+)/$', 'counter.views.event_detail'),
+#    (r'^showstats/(?P<event_key>[^\.^/]+)/$', 'counter.views.showstats'),
+#    (r'^stat/(?P<stat_key>[^\.^/]+)/$', 'counter.views.stat'),
+#   
+#   	(r'^contributor_stats/$', 'counter.views.Show_ContributorStat'),
+#    
+#    (r'^multisession-event/(?P<event_key>[^\.^/]+)/$', 'counter.views.multisession_event_detail'),	
+#    (r'^multisession-ratio/(?P<event_key>[^\.^/]+)/$', 'counter.views.multisession_event_ratio'),
+#	(r'^all_multisession/$', 'counter.views.all_multisession_events'),
+#	(r'^all_multisession_add/$', 'counter.views.all_multisession_events_add'),
+#	(r'^all/$', 'counter.views.all_events'),
+#	(r'^all/$', 'counter.views.all_events'),
+#	(r'^why/$',  'counter.views.why'),
+#	(r'^faq/$',  'counter.views.faq'),
+#	
+#	
+#
 
 
 	
